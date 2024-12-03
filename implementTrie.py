2a0d4fcd0f612b42501d609fa5174a37b3477ebc
LEGEND0WSKI <siddhesh.koli32@gmail.com>
2024-12-02 23:02:19 -0600

    1/3 Trie Done

---
+++ implementTrie.py
@@ -0,0 +1,50 @@
+# // Time Complexity :
+# // Space Complexity :
+# // Did this code successfully run on Leetcode :Yes
+# // Any problem you faced while coding this :
+
+class TrieNode:
+    def __init__(self):
+        self.children = [None] *26  # 26 size hashet at every elvel
+        self.isEnd = False          # if an active word 
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            i = ord(char) - ord('a')           
+            if not curr.children[i]:
+                curr.children[i] = TrieNode()       # create a trieNode
+            curr = curr.children[i]                 # next
+        curr.isEnd = True 
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for char in word:
+            i = ord(char) - ord('a')          
+            if not curr.children[i]:
+                return False
+            curr = curr.children[i]
+        return curr.isEnd
+        
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root        
+        for char in prefix:
+            i = ord(char) - ord('a')
+            if not curr.children[i]:
+                return False
+            curr = curr.children[i]
+        return True
+
+
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
